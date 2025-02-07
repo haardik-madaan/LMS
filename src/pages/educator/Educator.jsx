f89f6cd1a4e2c06b8efba6ad4f976981ae9ea3ee
haardik-madaan <madaanhaardik@gmail.com>
2025-02-08 03:10:28 +0530

    add routes for students and educators

--- src/pages/educator/Educator.jsx
+++ src/pages/educator/Educator.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Educator = () => {
   return (
-    <div>Educator</div>
+    <div>Educator
+
+    <div>
+      {<Outlet/>}
+    </div>
+    </div>
   )
 }
 
